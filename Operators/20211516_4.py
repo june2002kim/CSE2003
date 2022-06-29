f963c03fbfb33f5d9f9a43b79141061fecd43c7d
june020503 <108285472+june2002kim@users.noreply.github.com>
2022-06-29 21:40:37 +0900

    Create Operators
    Codes for training operators at python

---
+++ Operators/20211516_4.py
@@ -0,0 +1,12 @@
+from math import*
+
+score1=21.9
+score2=37
+score3=13.6
+
+avg=(score1+score2+score3)/3
+avg=round(avg, 2)
+print("average :", avg)
+
+avg=trunc(avg)
+print("average :", avg)
---
+++ Operators/20211516_6.py
@@ -0,0 +1,11 @@
+from math import pi
+
+h=2.57
+r=1.19
+
+s=2*pi*r*h+2*pi*r**2
+
+print("height:", round(h, 1))
+print("radius:", round(r, 1))
+print("surface area:", round(s, 4))
+
